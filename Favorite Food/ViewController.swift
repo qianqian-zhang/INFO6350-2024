8d688abef26b5e729b8c524f4c772f33ba6b377e
qianqian-zhang <zhang.qianqia@northeastern.edu>
2024-01-14 01:09:05 -0800

    Initial Commit

---
+++ Favorite Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Favorite Food
+//
+//  Created by Qianqian Zhang on 1/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
